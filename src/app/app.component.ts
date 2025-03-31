2259789ff336181c13d14bbb5ce951516e78b8c7
Ezequiel Ângelo <ezequiel_angelo_bbu@hotmail.com>
2025-03-31 16:33:55 -0400

    Criando um template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
